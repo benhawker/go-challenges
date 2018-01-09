be5c131b10c204b8363fb9d5dadf27e6ed241315
Ben Hawker <ben.c.hawker@gmail.com>
2018-01-09 16:02:00 +0000

    Update

--- biggest_diff.go
+++ biggest_diff.go
@@ -2,4 +2,42 @@
 // vs the previous element
 //
 // array = [1,4,3,6,7,5]
+// Should return 4 and 6.
 
+package main
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var array = []int{1,4,3,6,7,5}
+
+	// Diffs
+	diffMap := make(map[int][]int)
+	
+	// fmt.Println(array)
+
+	for i := 1; i < len(array); i++ {
+		difference := (array[i] - array[i-1])
+
+		diffMap[difference] = append(diffMap[difference], i)
+	} 
+
+ 	// To store the keys in slice in sorted order
+  var keys []int
+  for k := range diffMap {
+    keys = append(keys, k)
+  }
+  
+  sort.Ints(keys)
+
+  fmt.Println("The peak difference elements are:")
+
+  var results = diffMap[(keys[(len(keys)-1)])]
+
+  for i := 0; i < (len(results)); i++ {
+  	var value = results[i]
+  	fmt.Println(array[value])
+  }
+}
