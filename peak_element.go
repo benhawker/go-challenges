5dec76d0063a9b0d39c0aaffb455a2b2fafa3a36
Ben Hawker <ben.c.hawker@gmail.com>
2018-01-09 15:58:17 +0000

    Peak element

--- peak_element.go
+++ peak_element.go
@@ -3,3 +3,20 @@
 // For example - In Array {1,4,3,6,7,5}, 4 and 7 are peak elements.
 // We need to return any one peak element.
 
+
+package main
+import "fmt"
+
+func main() {
+	// array := [4]int{1, 2, 3, 4}
+	// var productArray [4]int
+
+	var array = {1,4,3,6,7,5}
+
+	// Declares an empty slice to handle the
+	s := make([]string, 3)
+
+	// 
+
+
+}
